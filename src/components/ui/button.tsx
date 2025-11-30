fd5457515e95e9cb9367d7900f36d98c28b013df
nabeff <neffina8@gmail.com>
2025-11-30 20:46:53 +0100

    new updates

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
         default: 'bg-primary text-primary-foreground hover:bg-primary/90',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         ghost: 'hover:bg-card hover:text-accent-foreground',
-        link: 'items-start justify-start underline-offset-4 hover:underline',
+        link: 'items-start justify-start ',
         outline: 'border border-border bg-background hover:bg-card hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
 
